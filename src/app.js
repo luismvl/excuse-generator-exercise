9919afa66c53ef7c411a1cc2ed1a57e98e8ab640
Luis Vela <luismvl114@gmail.com>
2023-03-06 13:48:54 +0000

    projeect finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function getRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(
+    what
+  )} ${getRandom(when)}`;
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
